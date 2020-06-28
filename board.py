f26fff5b5d5e482549ef4ed5bd06d82a1e973088
Geoffrey Hughes <geoffreyhughes>
2020-06-28 03:35:01 -0700

    adding click coords

--- board.py
+++ board.py
@@ -22,3 +22,22 @@ plt.yticks(y, y_squares)
 
 plt.imshow(chessboard, cmap='binary')
 plt.show()
+
+
+
+def on_button_press(event):
+   n = 300 #number of timepoints
+   t_sim = range(0, stop=1, length=n)
+   t_span = (0.0, 300.0)
+   x, y = event.xdata, event.ydata
+   u0 = [x, y]
+   prob = ODEProblem(f, u0, t_span)
+   sol = solve(prob)
+   plt.plot(sol(t_sim, idxs=1), sol(t_sim, idxs=2), "k-", markersize=10) # path
+   plt.plot(sol(t_sim, idxs=1)[1], sol(t_sim, idxs=2)[2], "*", markersize=10) # start
+   plt.plot(sol(t_sim, idxs=1)[end-1], sol(t_sim, idxs=2)[end], "o", markersize=10) # end
+   fig.canvas.draw()
+
+
+plt.canvas.mpl_connect("button_press_event", on_button_press)
+plt.show()
